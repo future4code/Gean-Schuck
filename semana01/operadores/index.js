fdaf575f4bc372606139ca17a8f510b922b94621
geangv <geangv@outlook.com>
2021-06-15 16:37:05 -0300

    .

---
+++ semana01/operadores/index.js
@@ -0,0 +1,47 @@
+// <----------------------- EXERCICIO 1 ----------------> //
+// const idadeUsuario=Number(prompt("Qual a sua idade ?"));
+// const idadeUsuario2=Number(prompt("Qual a idade do seu melhor amigo ou amiga ?"));
+
+// const resultado= idadeUsuario >= idadeUsuario2
+
+// console.log("Sua idade é maior do que a do seu melhor amigo?", resultado)
+
+
+// <--------------------- EXERCICIO 2 ----------------> //
+
+// const numeroPar=Number(prompt("Digite um numero par por favor: "));
+
+// const resultado = numeroPar % 2
+
+// console.log("Resto da divisao: ", resultado)
+
+// //Se eu nao estiver errado, todos os pares o resultado da zero, quando e numero impar ele reflete o proprio numero
+
+
+// <--------------------- EXERCICIO 3 ---------------------> //
+
+// const idadeUsuario=Number(prompt("Qual ano voce nasceu ?"));
+// const resultadoAno= idadeUsuario - 2021
+// console.log("voce tem: ", resultadoAno, "anos")
+
+//1
+//const idadeUsuario=Number(prompt("Qual a sua idade ?"));
+//console.log("Sua idade em meses e: ", idadeUsuario*12)
+
+//2
+// const idadeUsuario=Number(prompt("Qual a sua idade ?"));
+// console.log("Sua idade em dias: ", idadeUsuario*12*30)
+
+//3
+// const idadeUsuario=Number(prompt("Qual a sua idade ?"));
+// console.log("Sua idade em horas: ", idadeUsuario*12*30*60)
+
+
+// <---------------------------- EXERCICIO 4 ------------------> //
+// const numeroUm=Number(prompt("Digite um Numero: "))
+// const numeroDois=Number(prompt("Digite um segundo Numero: "))
+
+// console.log("O primeiro numero e maior que o segundo ?", numeroUm > numeroDois)
+// console.log("O primeiro numero e igual ao segundo ?", numeroUm === numeroDois)
+// console.log("O primeiro numero e divisivel pelo segundo ?")
+// console.log("O segundo numero e divisivel pelo primeiro ?")
